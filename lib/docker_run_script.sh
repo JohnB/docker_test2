95212d2e0c3242d5e3cbf707e8eb4d796d7f7f91
JohnB <john.baylor@gmail.com>
2019-03-11 22:17:15 -0700

    Once more, with 'sh' and an echoey shell script.

--- lib/docker_run_script.sh
+++ lib/docker_run_script.sh
@@ -1,3 +1,5 @@
-#!/bin/ash
-cd /opt/app/docker_test2 && mix phx.server
+#!/bin/sh
 
+echo "Starting phoenix server..."
+cd /opt/app/docker_test2 && mix phx.server
+echo "...and now its Watch has ended."
